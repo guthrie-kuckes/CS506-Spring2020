4b56d1d1fb033fa7f8b7b01e95a4e026fe274fb9
guthrie-kuckes <guskuckes@gmail.com>
2020-01-27 17:34:09 -0500

    a nice museum with some artists and frames

--- 01-python/leisure/museum.py
+++ 01-python/leisure/museum.py
@@ -1,3 +1,32 @@
+
+
+artists=["Rodin", "Van Gogh", "Pollack", "Miró "]
+
 def draw_museum():
-    print("museum not found")
+    print("Welcome to the Boston University Computer Science Museum")
+    
+    print("\n\n")
+    for artist in artists:
+        printFrame();
+        print(artist)
+        print()
+    print("\n\n")
+
+    return
+
+
+
+def printFrame(width=10, height=10):
+    for i in range(width):
+        print("=",end="")
+    print()
+    middleString = "|"
+    for i in range(width - 1):
+        middleString=middleString + " "
+    middleString+= "|"
+    for i in range(height):
+        print(middleString)
+    for i in range(width):
+        print("=",end="")
+    print()
     return
