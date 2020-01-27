151adbda8392cc490f35aa446b20f71ead36cb33
guthrie-kuckes <guskuckes@gmail.com>
2020-01-27 17:34:09 -0500

    a nice museum with some artists and frames

--- 01-python/leisure/museum.py
+++ 01-python/leisure/museum.py
@@ -1,21 +1,32 @@
-def draw_museum(height = 3):
-	"""
-	Draw a museum with specified height.
 
-	Parameters:
-	height(int): the height of the pillars of the museum.
 
-	Returns:
-	None
+artists=["Rodin", "Van Gogh", "Pollack", "Miró "]
 
-	"""
+def draw_museum():
+    print("Welcome to the Boston University Computer Science Museum")
+    
+    print("\n\n")
+    for artist in artists:
+        printFrame();
+        print(artist)
+        print()
+    print("\n\n")
 
-	print("   ==============")
-	print("  =              =")
-	print(" =                = ")
-	print("=                  =")
-	print("====================")
-	for _ in range(height):
-		print("|| || || || || || ||")
-	print("====================")
-	return
+    return
+
+
+
+def printFrame(width=10, height=10):
+    for i in range(width):
+        print("=",end="")
+    print()
+    middleString = "|"
+    for i in range(width - 1):
+        middleString=middleString + " "
+    middleString+= "|"
+    for i in range(height):
+        print(middleString)
+    for i in range(width):
+        print("=",end="")
+    print()
+    return
